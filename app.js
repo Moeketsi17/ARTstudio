a70f88e013233434dbcbdfc50f2fba334f6d1265
moeketsi <raymondkxng17@gmail.com>
2022-02-17 15:04:58 +0200

    image error in cnsole

--- app.js
+++ app.js
@@ -1,12 +1,24 @@
+// sticky nav
+// sticky nav
+window.addEventListener('scroll', function(){
+    let nav = document.querySelector('.nav-bar');
+    nav.classList.toggle('sticky', window.scrollY > 0)
+
+})
+
 
 // nav animate
 // nav animate
 const burger = document.querySelector('.burger');
 const navLinks = document.querySelector('.nav-links')
-const linkFade = document.querySelectorAll('.nav-links li a')
+const linkFade = document.querySelectorAll('.nav-links li')
+const logo = document.querySelector('logo')
 
 burger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
+    burger.classList.toggle('toggle');
+    logo.classList.toggle('enabled')
+
 
     // navlink fade in
     linkFade.forEach((link, index) => {
@@ -20,9 +32,27 @@ burger.addEventListener('click', () => {
 
 })
 
+// image animation
+// image animation
+let imageItems = [...document.querySelectorAll('.img-wrap')];
+let titles = [...document.querySelectorAll('.img-text')];
 
+let options = {
+    rootmargin: '0px',
+    threshold: 0.2
+}
 
+let setItemActive = (entries => {
+    console.log(entries)
+});
 
+let observer = new IntersectionObserver(setItemActive, options);
+
+imageItems.forEach((item, idx) => {
+    item.children[0].style.backgroundImage = `url(./images${idx+1}.jpeg)`;
+    idx % 2 == 0 ? item.style.left = '40%' : item.style.left = '5%';
+    observer.observe(item)
+})
 
 
 
