553e3fac980f8504a6810ab8f421f5ab4fbfeb83
moeketsi <raymondkxng17@gmail.com>
2022-03-10 14:36:52 +0200

    commit

--- app.js
+++ app.js
@@ -1,58 +1,81 @@
 // sticky nav
 // sticky nav
-window.addEventListener('scroll', function(){
-    let nav = document.querySelector('.nav-bar');
-    nav.classList.toggle('sticky', window.scrollY > 0)
-
-})
+// window.addEventListener('scroll', function(){
+//     let nav = document.querySelector('.nav-bar');
+//     nav.classList.toggle('sticky', window.scrollY > 0)
+// });
 
 
 // nav animate
 // nav animate
 const burger = document.querySelector('.burger');
 const navLinks = document.querySelector('.nav-links')
-const linkFade = document.querySelectorAll('.nav-links li')
+const linkFade = document.querySelectorAll('.nav-links li a')
 const logo = document.querySelector('logo')
 
 burger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
-    burger.classList.toggle('toggle');
-    logo.classList.toggle('enabled')
+    burger.classList.toggle('open');
+    logo.classList.toggle('enabled');
 
-
-    // navlink fade in
-    linkFade.forEach((link, index) => {
-        if(link.style.animation){
-            link.style.animation = ''
-        } else {
-            link.style.animation = `navLinkFade 0.6s ease forwards ${index / 4 + 0.3}s `
-        }
-        console.log(index)
-    })
-
-})
-
-// image animation
-// image animation
-let imageItems = [...document.querySelectorAll('.img-wrap')];
-let titles = [...document.querySelectorAll('.img-text')];
-
-let options = {
-    rootmargin: '0px',
-    threshold: 0.2
-}
-
-let setItemActive = (entries => {
-    console.log(entries)
+    
 });
 
-let observer = new IntersectionObserver(setItemActive, options);
 
-imageItems.forEach((item, idx) => {
-    item.children[0].style.backgroundImage = `url(./images${idx+1}.jpeg)`;
-    idx % 2 == 0 ? item.style.left = '40%' : item.style.left = '5%';
-    observer.observe(item)
-})
+ // smooth scroll 
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#js-scroll'),
+    smooth: true,
+    smoothMobile: true,
+    inertia: 0.75
+});
+
+
+anime.timeline()
+    .add({
+        targets: '.anime-js-title .letter',
+        translateY: [60, 0],
+        translateZ: 0,
+        opacity: [0, 1],
+        easing: "easeOutExpo",
+        duration: 1200,
+        delay: (el, i) => 600 + 30 * i
+    });
+
+// project subtitle animation 
+TweenMax.from(".project__container-subtitle", 2, {
+    y: 10,
+    opacity: 0,
+    ease: Expo.easeInOut
+});
+
+// project image fade 
+TweenMax.from(".project__image-one", 2, {
+    opacity: 0,
+    ease: Expo.easeInOut
+});
+
+// image animation
+// image animation
+// let imageItems = [...document.querySelectorAll('.img-wrap')];
+// let titles = [...document.querySelectorAll('.img-text')];
+
+// let options = {
+//     rootmargin: '0px',
+//     threshold: 0.2
+// }
+
+// let setItemActive = (entries => {
+//     console.log(entries)
+// });
+
+// let observer = new IntersectionObserver(setItemActive, options);
+
+// imageItems.forEach((item, idx) => {
+//     item.children[0].style.backgroundImage = `url(./images${idx+1}.jpeg)`;
+//     idx % 2 == 0 ? item.style.left = '40%' : item.style.left = '5%';
+//     observer.observe(item)
+// })
 
 
 
